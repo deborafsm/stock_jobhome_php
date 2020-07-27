75c5e230c7a22336ef59b189fd352b3cba373cd6
deborafsm <deborafreiresoares@outlook.com>
2020-07-27 14:32:51 -0300

    Stock

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="sty.css">
+
+    <title></title>
+</head>
+
+<body>
+
+    <header class="cabecalho">
+        <div class="job">
+            <img src="img\Logo-JobHome-br-1.png" alt="">
+        </div>
+       
+    </header>
+    <main class="principal">
+    
+        
+
+    </main>
+    <footer class="rodape">
+        Debs! ® Pixie  <?= date('Y') ?>
+    </footer>
+
+</body>
+
+</html>
